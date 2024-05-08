35483fddff76702966216563ba0be864f9c2702a
Johan Nystrom <johan@monomorphic.org>
2024-05-08 10:43:07 +0900

    Refactor dynamic classification to not compute the approximate taxon set if a gold set-based classification was requested (saving a lot of work in such a case)

--- src/main/scala/com/jnpersson/slacken/Dynamic.scala
+++ src/main/scala/com/jnpersson/slacken/Dynamic.scala
@@ -36,11 +36,6 @@ class Dynamic[Record](base: TaxonomicIndex[Record], genomes: Inputs, taxonLabels
       as[(Taxon, Long)].collect()
   }
 
-  /** Possibly add a boolean variable that decides if step1AggregateTaxa is run on the TaxaAtRank set or the goldStandardTaxaSet
-  * */
-  def twoStepClassify(subjects: Dataset[InputFragment]): Dataset[(SeqTitle, Array[TaxonHit])] =
-    reclassify(subjects, step1AggregateTaxa(subjects))
-
   /** Perform two-step classification, writing the final results to a location.
    * @param inputs Subjects to classify (reads)
    * @param outputLocation Directory to write reports and classifications in
@@ -52,34 +47,50 @@ class Dynamic[Record](base: TaxonomicIndex[Record], genomes: Inputs, taxonLabels
     base.classifyHitsAndWrite(hits, outputLocation, cpar)
   }
 
-  /** Reclassify reads with a dynamic index. This produces the final result.
+  /** Find an estimated taxon set in the given reads (to be classified),
+   * emphasising recall over precision.
    */
-  def reclassify(subjects: Dataset[InputFragment], taxonCounts: Array[(Taxon, Long)]): Dataset[(SeqTitle, Array[TaxonHit])] = {
+  def findTaxonSet(subjects: Dataset[InputFragment]): mutable.BitSet = {
+    val taxonCounts = step1AggregateTaxa(subjects)
     val taxonTotal = taxonCounts.iterator.map(_._2.toDouble).sum
 
     val keepTaxa = mutable.BitSet.empty ++ taxonCounts.iterator.filter(_._2/taxonTotal >= taxonMinFraction).map(_._1)
 
     val taxaAtRank = keepTaxa.
       filter(t => taxonomy.depth(t) >= reclassifyRank.depth)
-//    flatMap(t => List(t, taxonomy.ancestorAtLevel(t, reclassifyRank))).
+    //    flatMap(t => List(t, taxonomy.ancestorAtLevel(t, reclassifyRank))).
 
     println(s"Initial scan produced ${keepTaxa.size} taxa, filtered at $reclassifyRank to ${taxaAtRank.size} taxa")
+    taxaAtRank
+  }
 
-    // can make this more efficient by not computing the taxaAtRank if the classification with the GoldSet has been
-    // requested @nishad
+  /** Reclassify reads using a dynamic index. This produces the final result.
+   * The dynamic index is built from a taxon set, which can be either supplied (a gold standard set)
+   * or detected using a heuristic.
+   */
+  def twoStepClassify(subjects: Dataset[InputFragment]): Dataset[(SeqTitle, Array[TaxonHit])] = {
+
     val taxonSet = goldStandardTaxonSet match {
-      case Some((path,classifyWithGoldset)) =>
+      case Some((path,classifyWithGoldSet)) =>
         val goldSet = mutable.BitSet.empty ++ spark.read.csv(path).map(x => x.getString(0).toInt).collect()
-        val tp = taxaAtRank.intersect(goldSet).size
-        val fp = (taxaAtRank -- taxaAtRank.intersect(goldSet)).size
-        val fn = (goldSet -- taxaAtRank.intersect(goldSet)).size
-        val precision = tp.toDouble/(tp+fp)
-        val recall = tp.toDouble/goldSet.size
-        println(s"True Positives: $tp, False Positives: $fp, False Negatives: $fn, Precision: $precision, Recall: $recall")
-        if(classifyWithGoldset) goldSet else taxaAtRank
+        if(classifyWithGoldSet) {
+          goldSet
+        } else {
+          import com.jnpersson.discount.spark.Output.formatPerc
+          //Only compare the gold standard set to the detected set, use the latter for classification
+          val taxaAtRank = findTaxonSet(subjects)
+          val tp = taxaAtRank.intersect(goldSet).size
+          val fp = (taxaAtRank -- taxaAtRank.intersect(goldSet)).size
+          val fn = (goldSet -- taxaAtRank.intersect(goldSet)).size
+          val precision = tp.toDouble/(tp+fp)
+          val recall = tp.toDouble/goldSet.size
+          println(s"True Positives: $tp, False Positives: $fp, False Negatives: $fn, " +
+            s"Precision: ${formatPerc(precision)}, Recall: ${formatPerc(recall)}")
+          taxaAtRank
+        }
 
       case None =>
-        taxaAtRank
+        findTaxonSet(subjects)
     }
 
     //Dynamically create a new index containing only the identified taxa and their descendants
