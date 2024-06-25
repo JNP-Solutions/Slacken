8b85c6110b471fd2054071cd4af6f1bef84fbaf3
PathogeNish <105763865+PathogeNish@users.noreply.github.com>
2024-06-25 10:29:32 +0530

    Work In Progress on BrackenWeights

--- src/main/scala/com/jnpersson/slacken/BrackenWeights.scala
+++ src/main/scala/com/jnpersson/slacken/BrackenWeights.scala
@@ -1,12 +1,13 @@
 package com.jnpersson.slacken
 
-import com.jnpersson.discount
+import com.jnpersson.{discount, slacken}
 import com.jnpersson.discount.{NTSeq, SeqLocation, SeqTitle}
 import com.jnpersson.discount.hash.InputFragment
 import com.jnpersson.discount.spark.AnyMinSplitter
 import com.jnpersson.discount.util.NTBitArray
 import com.jnpersson.slacken.TaxonomicIndex.{classify, getTaxonLabels}
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.functions.collect_list
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession, functions}
 
 import scala.collection.{BitSet, mutable}
 
@@ -19,14 +20,21 @@ object TaxonFragment {
 
 }
 
+/**
+ *
+ * @param taxon
+ * @param nucleotides
+ * @param id the fragment id
+ */
 final case class TaxonFragment(taxon: Taxon, nucleotides: NTSeq, id: String) {
 
   /**
    * Returns all distinct minimizers in the nucleotide sequence
+   *
    * @param splitter
    * @return
    */
-  def distinctMinimizers(splitter: AnyMinSplitter)= {
+  def distinctMinimizers(splitter: AnyMinSplitter) = {
     splitter.superkmerPositions(nucleotides, addRC = false).map(_._2).toArray.distinct.iterator.map(_.data)
   }
 
@@ -41,12 +49,13 @@ final case class TaxonFragment(taxon: Taxon, nucleotides: NTSeq, id: String) {
 
   /**
    * Generate reads from the fragment then classify them according to the lca's.
+   *
    * @param minimizers
    * @param lcas
    * @return
    */
-  def readClassifications(taxonomy: Taxonomy, minimizers:Array[Array[Long]], lcas: Array[Taxon],
-                          splitter: AnyMinSplitter, readLen: Int):Iterator[(Taxon,Long)] = {
+  def readClassifications(taxonomy: Taxonomy, minimizers: Array[Array[Long]], lcas: Array[Taxon],
+                          splitter: AnyMinSplitter, readLen: Int): Iterator[(Taxon, Long)] = {
 
 
     val encodedMinimizers = minimizers.map(m => NTBitArray(m, splitter.priorities.width))
@@ -61,21 +70,25 @@ final case class TaxonFragment(taxon: Taxon, nucleotides: NTSeq, id: String) {
     // true ordinal not needed for this use case
     val classifications = reads.flatMap { r =>
       val taxonHits = splitter.superkmerPositions(r, addRC = false).map(s =>
-        TaxonHit(s._2.data, bogusOrdinal, lcaLookup(s._2), s._3-(k-1)))
+        TaxonHit(s._2.data, bogusOrdinal, lcaLookup(s._2), s._3 - (k - 1)))
 
       TaxonomicIndex.classify(taxonomy, taxonHits.toArray, confidenceThreshold, k, cpar)
     }
-    ???
 
-    // generate reads from fragment: Get taxon genome -->
-    // Break genome up into read fragments and convert to Dataset[InputFragment] -->
-    // pass to classify with the minimizers
-
+    val counted = mutable.Map.empty[Taxon, Long].withDefaultValue(0)
+    for {c <- classifications} counted(c) += 1
+    counted.iterator
   }
 
 }
 
-class BrackenWeights(keyValueIndex: KeyValueIndex)(implicit val spark: SparkSession) {
+/**
+ *
+ * @param buckets       minimizer LCAs to classify genomes against.
+ * @param keyValueIndex used to collect minimizer parameters, taxonomy, splitter
+ * @param spark
+ */
+class BrackenWeights(buckets: DataFrame, keyValueIndex: KeyValueIndex, readLen: Int)(implicit val spark: SparkSession) {
 
   import spark.sqlContext.implicits._
 
@@ -88,6 +101,17 @@ class BrackenWeights(keyValueIndex: KeyValueIndex)(implicit val spark: SparkSess
     val fragments = idSeqDF.join(titlesTaxa, idSeqDF("header") === titlesTaxa("header")).
       select("taxon", "nucleotides", "location", "header").as[(Taxon, NTSeq, SeqLocation, SeqTitle)].
       map(x => TaxonFragment(x._1, x._2, x._4 + x._3))
+    val bcSplit = keyValueIndex.bcSplit
+    val bcTaxonomy = keyValueIndex.bcTaxonomy
+    val withMins = fragments.flatMap { x =>
+        x.distinctMinimizers(bcSplit.value).map(m => (x, m))
+      }.toDF("fragment", "minimizer").
+      join(buckets, keyValueIndex.idColumnNames).
+      groupBy("fragment.id").agg(functions.first("fragment"), collect_list("minimizer"), collect_list("taxon")).
+      as[(String, slacken.TaxonFragment, Array[Array[Long]], Array[Taxon])].
+      flatMap { case (id, f, m, t) =>
+        f.readClassifications(bcTaxonomy.value, m, t, bcSplit.value, readLen)
+      }
 
   }
 }
