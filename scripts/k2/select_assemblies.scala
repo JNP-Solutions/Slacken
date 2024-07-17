07b5cada5d3b8bffa102a854116dfddf26b7ed9c
Johan Nystrom <johan@monomorphic.org>
2024-07-17 18:29:22 +0900

    For the library download and build scripts, add a function to filter the genomes at different levels. In particular, add "prefer_complete" which supplements complete genomes with incomplete genomes only when the former are not present for a taxon.

---
+++ scripts/k2/select_assemblies.scala
@@ -0,0 +1,42 @@
+import scala.io.Source
+
+/* Script to filter assembly_summary.txt. For use from download_genomic_library.sh. */
+
+case class Record(taxid: Int, asmLevel: String) {
+  //This definition of "complete" comes from Kraken2's rsync_from_ncbi.pl
+  def complete: Boolean = asmLevel == "Complete Genome" ||
+    asmLevel == "Chromosome"
+}
+
+val mode = args(0)
+val file = args(1)
+
+def lines = Source.fromFile(file).getLines().filter(! _.startsWith("#"))
+
+def toRecord(l: String) = {
+  val fs = l.split("\t")
+  Record(fs(5).toInt, fs(11))
+}
+
+def records = lines.map(toRecord)
+
+//taxa that have some complete genome (or chromosomes)
+val completeTaxa = scala.collection.mutable.BitSet.empty ++ records.filter(_.complete).map(_.taxid)
+
+for { l <- lines } {
+  mode match {
+    case "all" =>
+      println(l)
+    case "complete" =>
+      if (toRecord(l).complete)
+        println(l)
+    case "prefer_complete" =>
+      val rec = toRecord(l)
+      if (rec.complete || !completeTaxa.contains(rec.taxid))
+        println(l)
+    case "incomplete" =>
+      val rec = toRecord(l)
+      if (!completeTaxa.contains(rec.taxid))
+        println(l)
+  }
+}
