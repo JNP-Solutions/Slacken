e91c9ea2cd37df6bdb7fef7362fd33339456fb2f
PathogeNish <105763865+PathogeNish@users.noreply.github.com>
2024-07-22 17:36:21 +0530

    Added cases for choosing kmer strategies and edited KmerHashStrategies.scala classes to include case of null input

--- src/main/scala/com/jnpersson/slacken/KmerHashStrategies.scala
+++ src/main/scala/com/jnpersson/slacken/KmerHashStrategies.scala
@@ -6,8 +6,8 @@ class KmerStrategy1 extends Hash.Strategy[Array[Long]]{
   override def hashCode(minimizer: Array[Long]): Int = 31 + (minimizer(0) ^ (minimizer(0) >>> 32)).toInt
 
   override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
-    println(s"EQUALS ${m1}, ${m2}")
-    m1(0) == m2(0)
+    if (m1 != null && m2 != null) m1(0) == m2(0)
+    else if (m1 == null && m2 == null) true else false
   }
 
 }
@@ -18,7 +18,11 @@ class KmerStrategy2 extends Hash.Strategy[Array[Long]]{
     31*(31+ (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) + (minimizer(1) ^ (minimizer(1) >>> 32)).toInt
   }
 
-  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = (m1(0) == m2(0)) && (m1(1)==m2(1))
+  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
+
+    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1))
+    else if (m1 == null && m2 == null) true else false
+  }
 
 }
 
@@ -30,7 +34,9 @@ class KmerStrategy3 extends Hash.Strategy[Array[Long]]{
   }
 
   override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
-    (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2))
+
+    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2))
+    else if (m1 == null && m2 == null) true else false
   }
 
 }
@@ -43,7 +49,9 @@ class KmerStrategy4 extends Hash.Strategy[Array[Long]]{
   }
 
   override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
-    (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2)) && (m1(3)==m2(3))
+
+    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2)) && (m1(3)==m2(3))
+    else if (m1 == null && m2 == null) true else false
   }
 
 }
