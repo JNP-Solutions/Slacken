0db55d6dcb82ffc9dc5b8fbc085073bdf2bd088c
PathogeNish <105763865+PathogeNish@users.noreply.github.com>
2024-07-23 10:42:16 +0530

    Changed class to final, updated logic for equals function

--- src/main/scala/com/jnpersson/slacken/KmerHashStrategies.scala
+++ src/main/scala/com/jnpersson/slacken/KmerHashStrategies.scala
@@ -1,57 +1,72 @@
 package com.jnpersson.slacken
 
 import it.unimi.dsi.fastutil.Hash
-class KmerStrategy1 extends Hash.Strategy[Array[Long]]{
+
+final class KmerStrategy1 extends Hash.Strategy[Array[Long]] {
 
   override def hashCode(minimizer: Array[Long]): Int = 31 + (minimizer(0) ^ (minimizer(0) >>> 32)).toInt
 
-  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
-    if (m1 != null && m2 != null) m1(0) == m2(0)
-    else if (m1 == null && m2 == null) true else false
+  override def equals(m1: Array[Long], m2: Array[Long]): Boolean = {
+    // Object2IntCustomHashMap.getInt does null comparisons by passing null values to m2.
+    // We ensure that m1 is never null
+    m2 != null && m1(0) == m2(0)
   }
 
 }
 
-class KmerStrategy2 extends Hash.Strategy[Array[Long]]{
+final class KmerStrategy2 extends Hash.Strategy[Array[Long]] {
 
   override def hashCode(minimizer: Array[Long]): Int = {
-    31*(31+ (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) + (minimizer(1) ^ (minimizer(1) >>> 32)).toInt
+    31 * (31 +
+      (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) +
+      (minimizer(1) ^ (minimizer(1) >>> 32)).toInt
   }
 
-  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
+  override def equals(m1: Array[Long], m2: Array[Long]): Boolean = {
 
-    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1))
-    else if (m1 == null && m2 == null) true else false
+    m2 != null &&
+      m1(0) == m2(0) &&
+      m1(1) == m2(1)
   }
 
 }
 
-class KmerStrategy3 extends Hash.Strategy[Array[Long]]{
+final class KmerStrategy3 extends Hash.Strategy[Array[Long]] {
 
   override def hashCode(minimizer: Array[Long]): Int = {
-    31*(31*(31+ (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) + (minimizer(1) ^ (minimizer(1) >>> 32)).toInt)
-    + (minimizer(2) ^ (minimizer(2) >>> 32)).toInt
+    31 * (31 * (31 +
+      (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) +
+      (minimizer(1) ^ (minimizer(1) >>> 32)).toInt) +
+      (minimizer(2) ^ (minimizer(2) >>> 32)).toInt
   }
 
-  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
+  override def equals(m1: Array[Long], m2: Array[Long]): Boolean = {
 
-    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2))
-    else if (m1 == null && m2 == null) true else false
+    m2 != null &&
+      m1(0) == m2(0) &&
+      m1(1) == m2(1) &&
+      m1(2) == m2(2)
   }
 
 }
 
-class KmerStrategy4 extends Hash.Strategy[Array[Long]]{
+final class KmerStrategy4 extends Hash.Strategy[Array[Long]] {
 
   override def hashCode(minimizer: Array[Long]): Int = {
-    31*(31*(31*(31+ (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) + (minimizer(1) ^ (minimizer(1) >>> 32)).toInt)
-    + (minimizer(2) ^ (minimizer(2) >>> 32)).toInt) + (minimizer(3) ^ (minimizer(3) >>> 32)).toInt
+    31 * (31 * (31 * (31 +
+      (minimizer(0) ^ (minimizer(0) >>> 32)).toInt) +
+      (minimizer(1) ^ (minimizer(1) >>> 32)).toInt) +
+      (minimizer(2) ^ (minimizer(2) >>> 32)).toInt) +
+      (minimizer(3) ^ (minimizer(3) >>> 32)).toInt
   }
 
-  override def equals(m1: Array[Long] , m2: Array[Long] ): Boolean = {
+  override def equals(m1: Array[Long], m2: Array[Long]): Boolean = {
 
-    if (m1 != null && m2 != null) (m1(0) == m2(0)) && (m1(1)==m2(1)) && (m1(2)==m2(2)) && (m1(3)==m2(3))
-    else if (m1 == null && m2 == null) true else false
+    m2 != null &&
+      m1(0) == m2(0) &&
+      m1(1) == m2(1) &&
+      m1(2) == m2(2) &&
+      m1(3) == m2(3)
   }
 
 }
