9497739e6e2ea486461557db1045b7ae66a3e8d1
Johan Nystrom <johan@monomorphic.org>
2024-07-30 13:35:54 +0900

    Delete some unused comparison scripts. Rename collate_metrics to join_metrics.

--- scripts/comparison/join_metrics.sh
+++ scripts/comparison/join_metrics.sh
@@ -2,7 +2,7 @@
 
 #Very simple script to combine the various sampleN_metrics.tsv files produced
 #when we compare classifications with a reference.
-#Usage: collate_metrics.sh sample1_metrics.tsv sample2_metrics.tsv ... > samples_metrics.tsv
+#Usage: join_metrics.sh sample1_metrics.tsv sample2_metrics.tsv ... > samples_metrics.tsv
 
 head -1 $1
 tail -q -n +2 $*
