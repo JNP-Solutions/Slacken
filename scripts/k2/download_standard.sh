8b44af863692f7b17244e14af741e70fe85a2d12
Johan Nystrom <johan@monomorphic.org>
2024-11-25 13:33:25 +0900

    Comments and clarifications for scripts/k2.
    Removed the "incomplete only" download mode for select_assemblies.scala as it doesn't seem useful.

--- scripts/k2/download_standard.sh
+++ scripts/k2/download_standard.sh
@@ -3,11 +3,13 @@
 #Script to download and pre-process sequences for the Kraken 2 standard library (nucleotide form only, not protein).
 #Reference: standard_installation.sh in kraken2
 
-#Example environment:
-#KRAKEN2_DB_NAME= (wherever the database is)
-#KRAKEN2_DIR= (wherever these scripts are located)
+#See README.txt in this directory for guidance.
 
-export KRAKEN2_INCOMPLETE=
+#Example environment:
+#KRAKEN2_DB_NAME=(destination)
+#KRAKEN2_DIR=(wherever these scripts are located)
+
+export KRAKEN2_FILTER_LEVEL=complete
 export KRAKEN2_PROTEIN_DB=
 export KRAKEN2_SKIP_MAPS=
 export KRAKEN2_USE_FTP=
@@ -26,7 +28,7 @@ download_genomic_library.sh viral
 download_genomic_library.sh plasmid
 download_genomic_library.sh UniVec_Core
 
-#The human genome is not low-complexity masked
+#The human genome is not low complexity masked
 export KRAKEN2_MASK_LC=""
 download_genomic_library.sh human
 
