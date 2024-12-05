c4df9e6da0f453e4ce30436018ce0a595bca5ab6
PathogeNish <105763865+PathogeNish@users.noreply.github.com>
2024-12-05 11:41:00 +0530

    Added scala test for resolve tree with taxon hits from a single lineage

--- src/test/scala/com/jnpersson/slacken/ClassifierProps.scala
+++ src/test/scala/com/jnpersson/slacken/ClassifierProps.scala
@@ -14,16 +14,40 @@ class ClassifierProps extends AnyFunSuite with ScalaCheckPropertyChecks with Mat
     Gen.listOfN(kmers / sizePerHit, taxonHits(Array(taxon), sizePerHit)).map(_.toArray)
   }
 
+  def readHitsLineage(taxonomy: Taxonomy, taxon: Taxon, kmers: Int): Gen[Array[TaxonHit]] = {
+    val sizePerHit = 5
+    val lineage = taxonomy.pathToRoot(taxon).toArray
+    Gen.listOfN(kmers / sizePerHit, taxonHits(lineage, sizePerHit)).map(_.toArray)
+  }
+
   test("resolveTree") {
-    forAll(taxonomies(10), Gen.choose(10, 100)) { (t, k) =>
-      val taxon = t.taxa.next()
-      val lca = new LowestCommonAncestor(t)
-      forAll(readHits(taxon, k)) { hits =>
-        val r = lca.resolveTree(TaxonCounts.fromHits(hits), 0)
-        if (hits.size == 0)
-          r should equal(Taxonomy.NONE)
-        else
-          r should equal(taxon)
+    forAll(taxonomies(100), Gen.choose(10, 100), Gen.choose(1, 99)) { (t, k, taxon) =>
+      whenever(taxon > 0) {
+        val lca = new LowestCommonAncestor(t)
+        forAll(readHits(taxon, k)) { hits =>
+          val r = lca.resolveTree(TaxonCounts.fromHits(hits), 0)
+          if (hits.size == 0)
+            r should equal(Taxonomy.NONE)
+          else
+            r should equal(taxon)
+        }
+      }
+    }
+  }
+
+  test("resolveTree lineage") {
+    forAll(taxonomies(100), Gen.choose(10, 100), Gen.choose(1, 99)) { (t, k, taxon) =>
+      // prevent taxon from shrinking below 1
+      whenever(taxon > 0) {
+        val lca = new LowestCommonAncestor(t)
+        forAll(readHitsLineage(t, taxon, k)) { hits =>
+          val lowestHit = hits.sortBy(hit => t.depth(hit.taxon)).last.taxon
+          val r = lca.resolveTree(TaxonCounts.fromHits(hits), 0)
+          if (hits.size == 0)
+            r should equal(Taxonomy.NONE)
+          else
+            r should equal(lowestHit)
+        }
       }
     }
   }
